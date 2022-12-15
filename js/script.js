478ace8c262c6f4357a7494b566b9c27a1820f35
Ivan <vanja36484@yandex.ru>
2022-12-15 12:58:39 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+"use strict";
+
+
+let number = 5;
+const leftBorderWidth = 1;
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+console.log(obj);
+
+console.log(name2);
+var name2 = 'Ivan';
+
+{
+    var result = 50;
+}
+console.log(result);
+
+const object = {
+    age: 25,
+    name3: 'John',
+    isMarried: false
+    
+};
+console.log(object.name3);
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.jpg'];
+console.log(arr[2]);
+const object2 = {
+    Anna: 500,
+    'Alce': 800
+};
+const arr1 = ['a', 'b', 'c', 4];
+const arrobj = {
+    a: 'a',
+    1: 'b',
+    2: 'c'
+};
+arrobj.b = '1234';
+console.log(arrobj['b']);
+console.log(arr1[1]);
+// alert('Hello');
+// const results = confirm("Are you here?");
+// console.log(results);
+// const answer = prompt("Вам есть 18?", "18"); 
+// console.log(answer);
+const answers = [];
+// answers[0] = prompt("Как ваше имя?","");
+// answers[1] = prompt("Как ваша фамилия?","");
+// answers[2] = prompt("Сколько вам лет?","");
+
+// document.write(answers);
+console.log(typeof(answers));
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+alert(`Привет, ${user}`);
+
+
+
+
+
+
